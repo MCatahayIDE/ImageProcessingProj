cd7bf5f27512d7f0c6f31621eec2c9a807358973
Rich Soong <rsoong1@uci.edu>
2024-11-23 22:39:09 -0800

    update Cute_dog.jpg -> cute_dog.png

--- image_processing.m
+++ image_processing.m
@@ -1,5 +1,5 @@
 
-inputImage = imread('noisy_image.png'); 
+inputImage = imread('cute_dog.png'); 
 inputImage = im2double(inputImage); % Normalize pixel values to the range [0,1]
 
 % types of blur kernels
@@ -15,37 +15,38 @@ sigma = 3; % Standard deviation for Gaussian filter
 filter_size = 2 * ceil(3 * sigma) + 1; % Filter size (usually 6*sigma rounded up to the nearest odd integer)
 gaussianBlurKernel = fspecial('gaussian', filter_size, sigma);
 
-blurredImage = convn(inputImage, gaussianBlurKernel);
+blurredImage = convn(inputImage, sharpenKernel);
 
 
 % Display the original and blurred images
 figure;
-subplot(1, 2, 1);
+subplot(2, 1, 1);
 imshow(inputImage);
 title('Original Image');
 
-subplot(1, 2, 2);
+subplot(2, 1, 2);
 imshow(blurredImage);
-title('Blurred Image');
+title('Output Image');
 
-% Save the blurred image if needed
-% imwrite(blurredImage, 'blurred_image.jpg');
+% Save the image if needed
+% imwrite(outputImage, 'output_image.jpg');
 
 
 %% testing custom conv
 
-blurredImage = conv_2dcolor(inputImage, gaussianBlurKernel);
+blurredImage = conv_2dcolor(inputImage, sharpenKernel);
 
 
 % Display the original and blurred images
 figure;
-subplot(1, 2, 1);
+subplot(2, 1, 1);
 imshow(inputImage);
+
 title('Original Image');
 
-subplot(1, 2, 2);
+subplot(2, 1, 2);
 imshow(blurredImage);
-title('Blurred Image');
+title('Output Image');
 
-% Save the blurred image if needed
-% imwrite(blurredImage, 'blurred_image.jpg');
+% Save the image if needed
+% imwrite(outputImage, 'output_image.jpg');
